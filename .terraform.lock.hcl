4044fd73d6d0595d6aff336b386e676549eeccd5
Mohamed Romdhani <m.romdhani@businesstraining.be>
2022-02-09 12:06:38 +0100

    Initial commit

---
+++ .terraform.lock.hcl
@@ -0,0 +1,38 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/azurerm" {
+  version = "2.95.0"
+  hashes = [
+    "h1:f8UaL5uARHkCRmEKjKPRMg8/j6IiLAS/bYhbmFb81gQ=",
+    "zh:09d2b8894e2c5df4e140d6ddf9224105b48b367afac6e10921c2571d53ae1d9c",
+    "zh:2974b8cd6633a8e6cb62f728fafdf1b6c656a27d3e4c050e94c142204c3893e4",
+    "zh:365ce52883a81f44021416a353d5370465317807344cf99a303718c97d226aa8",
+    "zh:54215ddfe1951ad705f0ce807851b8f3eb5aa2dc96a976e272f9ba027e239814",
+    "zh:5cfdacbf0d889cf22b8521eb9a732365b12f391cec1b3277c4ad1eaf40879ab6",
+    "zh:7086802525e280c6caf3538cd6cd72304acbdf85192756628db71a7d8d7aabc3",
+    "zh:8ea6d0d28120059d39e6a34bea8b7856f20896858fdef657dcfecd2c382cf3d1",
+    "zh:c21486433e5914403eca3344808c02ee5c5630256d9bc3f6f6750c90ecfedb5f",
+    "zh:dfebccd09c0d5f333d1a1a36bbd33b7abd5ff521143e81792ecf06dd3a6d0c0c",
+    "zh:e0b1d0190b0d5bd697d4398c5262864630bc36651fc73432bc7a3dc7bcb64758",
+    "zh:e410dc4301c27db2ea291b39e675b2ea741f058f0da01c7dc9d4accf61298dad",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/random" {
+  version = "3.1.0"
+  hashes = [
+    "h1:EPIax4Ftp2SNdB9pUfoSjxoueDoLc/Ck3EUoeX0Dvsg=",
+    "zh:2bbb3339f0643b5daa07480ef4397bd23a79963cc364cdfbb4e86354cb7725bc",
+    "zh:3cd456047805bf639fbf2c761b1848880ea703a054f76db51852008b11008626",
+    "zh:4f251b0eda5bb5e3dc26ea4400dba200018213654b69b4a5f96abee815b4f5ff",
+    "zh:7011332745ea061e517fe1319bd6c75054a314155cb2c1199a5b01fe1889a7e2",
+    "zh:738ed82858317ccc246691c8b85995bc125ac3b4143043219bd0437adc56c992",
+    "zh:7dbe52fac7bb21227acd7529b487511c91f4107db9cc4414f50d04ffc3cab427",
+    "zh:a3a9251fb15f93e4cfc1789800fc2d7414bbc18944ad4c5c98f466e6477c42bc",
+    "zh:a543ec1a3a8c20635cf374110bd2f87c07374cf2c50617eee2c669b3ceeeaa9f",
+    "zh:d9ab41d556a48bd7059f0810cf020500635bfc696c9fc3adab5ea8915c1d886b",
+    "zh:d9e13427a7d011dbd654e591b0337e6074eef8c3b9bb11b2e39eaaf257044fd7",
+    "zh:f7605bd1437752114baf601bdf6931debe6dc6bfe3006eb7e9bb9080931dca8a",
+  ]
+}
---
+++ main.tf
@@ -0,0 +1,48 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg_lab" {
+  name     = "my-resource-group-lab-06"
+  location = "West Europe"
+}
+
+module "linuxservers" {
+  source              = "Azure/compute/azurerm"
+  resource_group_name = azurerm_resource_group.rg_lab.name
+  vm_os_simple        = "UbuntuServer"
+  public_ip_dns       = ["my-nginx-server"] // change to a unique name per datacenter region
+  vnet_subnet_id      = module.network.vnet_subnets[0]  
+  
+  # Want to be sure that we have Ubuntu 20.04 ?
+
+  depends_on = [azurerm_resource_group.rg_lab]
+
+  vm_hostname                      = "mylinuxvm"
+  nb_public_ip                     = 1
+  remote_port                      = "22"
+  nb_instances                     = 1
+  vm_os_publisher                  = "Canonical"
+  vm_os_offer                      = "UbuntuServer"
+  vm_os_sku                        = "18.04-LTS"
+
+  boot_diagnostics                 = true
+  delete_os_disk_on_termination    = true
+  nb_data_disk                     = 1
+  data_disk_size_gb                = 20
+  data_sa_type                     = "Standard_LRS"    # "Premium_LRS"
+  enable_ssh_key                   = true
+  ssh_key_values                   = ["ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDFGXTjNzm9Ba1EZch4P67hGvN7hYUzid7323A8Lh796/TYFXeqQ/aWO9mNmWZKMjsrFwTfF80GpccNsBMoCUVaE3x4tvER94zccU51HNlTdSG1jxjy2jbXm91sxvG3blxpAYWiafJ7y6Sb/rQF5Zq7vs24nIzchn+zO2GCvMBm4Y6tvUC6Kkmq/8BysjEoHmRlTrhSpzmDRR6hgFQ2BUHGi120dBDbribk4m7TyaUfNqxV7LI90VbPAVTuBnvKmmJzGbK66kRP0kIMCY6fbI50sVAabDu/Ki5z9r0wiSlb38y9sXgQyMImr4r8rcrmI3iaS7dcGlhmTV/WIBRc44kJwB+I9OiFkn2/pviI/Z0pxSGjVk6P0FvwpX8IStOem0BM8F1KND0mabfIdShN1AW0vU1OQ3kchbgTOZRUvZqzCD6on46q2un+AodhdS8gle3kJaRuqpfn7T2OHgrqNPvkLPQEejawUEn/ZF8DbD3Jdiw/DpY8MJo9PTWKgsPE3Fc= student@ROME3-0"]
+  vm_size                          = "Standard_DS1_v2"
+  delete_data_disks_on_termination = true
+
+}
+
+module "network" {
+  source              = "Azure/network/azurerm"
+  resource_group_name = azurerm_resource_group.rg_lab.name
+  subnet_prefixes     = ["10.0.1.0/24"]
+  subnet_names        = ["subnet1"]
+
+  depends_on = [azurerm_resource_group.rg_lab]
+}
